278fd1aea22845414b85be346c696d68f18c1568
mattoranking <mattoranking@gmail.com>
2026-03-20 00:21:48 +0000

    feat(auth): add endpoints for auth
    Signed-off-by: mattoranking <mattoranking@gmail.com>

--- apps/backend/faskplusai/api.py
+++ apps/backend/faskplusai/api.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
 from faskplusai.user.endpoints import router as user_router
+from faskplusai.auth.endpoints import router as auth_router
 
 router = APIRouter(prefix="/v1")
 
-# /clients
+# /auth
+router.include_router(auth_router)
+# /user
 router.include_router(user_router)
